ba7fcf2cca7588d0dce7c305274b5749e7f06b78
kevin <kevin.liou@quantatw.com>
2024-01-29 20:10:34 +0800

    Fix argparse_function error

--- ReleasePkgLib/Argparse.py
+++ ReleasePkgLib/Argparse.py
@@ -4,14 +4,16 @@ from colorama import Fore
 
 
 def argparse_function(ver):
-    parser = argparse.ArgumentParser(prog='ReleasePkg.py', description='Tutorial')
-    parser.add_argument("-d", "--debug", help='Show debug message.', action="store_true")
-    parser.add_argument("-v", "--version", action="version", version=ver)
+    parser = argparse.ArgumentParser(prog = 'ReleasePkg.py', description = 'Tutorial')
+    parser.add_argument("-d", "--debug", help = 'Show debug message.', action = "store_true")
+    parser.add_argument("-v", "--version", action = "version", version = ver)
     args = parser.parse_args()
+    logging.debug(f"args: {args}")
     if args.debug:
         Debug_Format = "%(levelname)s, %(funcName)s: %(message)s"
-        logging.basicConfig(level=logging.DEBUG, format=Debug_Format) # Debug use flag
+        logging.basicConfig(level = logging.DEBUG, format = Debug_Format) # Debug use flag
         print(Fore.RED + "Enable debug mode...")
-        return "Debug mode"
-    if args.version:
-        return ver
+        args = "Debug mode"
+    else:
+        args = "Release mode"
+    return args
